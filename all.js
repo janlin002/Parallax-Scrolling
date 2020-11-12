c45d07edc3c568a5d4e36667dbd4cfd22764a6e3
janlin <123davidbill@gmail.com>
2020-11-12 09:04:09 +0800

    update

--- all.js
+++ all.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var showSkill=false;
     $('.scrollTop').click(function(e){
         e.preventDefault();
         var target=$(this).attr('href');
@@ -9,25 +10,54 @@ $(document).ready(function(){
     });
 
     $(window).scroll(function(){
-        var scrollPos =$(window).scrollTop();
+        var scrollPos = $(window).scrollTop();
+        var windowHeight=$(window).height();
         // console.log(scrollPos);
 
-        $('.scrollTop').each(function(){
-            var target=$(this).attr('href');
-            var targetPos=$(target).offset().top;
-            var targetHeight=$(target).outerHeight();//加上外誆
-            // console.log(target,targetPos,targetHeight);
-            if(targetPos -1 <= scrollPos && (targetPos+targetHeight)>scrollPos){
-                // console.log(target);
-                $('.scrollTop').removeClass('active');
-                $(this).addClass('active');
-            }else{
-                $(this).removeClass('active');
-            }
+        // $('.scrollTop').each(function(){
+        //     var target=$(this).attr('href');
+        //     var targetPos=$(target).offset().top;
+        //     var targetHeight=$(target).outerHeight();//加上外誆
+        //     console.log(target,targetPos,targetHeight);
+        //     if(targetPos -1 <= scrollPos && (targetPos+targetHeight)>scrollPos){
+        //         console.log(target);
+        //         $('.scrollTop').removeClass('active');
+        //         $(this).addClass('active');
+        //     }else{
+        //         $(this).removeClass('active');
+        //     }
             
-        });
-    });
+        // });
 
-   
-  
-});
+        $('.scrollTop').each(function(){
+            var target = $(this).attr('href'); 
+            var targetPos = $(target).offset().top;
+            var targetHeight = $(target).outerHeight();
+            if (targetPos - 1 <= scrollPos && (targetPos + targetHeight) > scrollPos){
+              $('.scrollTop').removeClass('active')
+              $(this).addClass('active');
+            } else {
+              $(this).removeClass('active')
+            }
+          });
+
+        var skillTop=$('#skills').position().top;
+        if(skillTop<=(scrollPos + windowHeight / 2) && !showSkill ){
+            $('#skills, .progress-bar').each(function(){
+                var thisValue=$(this).data('progress');
+                $(this).css('width',thisValue+'%');
+            })
+        }
+        $('.animated').each(function(){
+            var thisPos=$(this).offset().top;
+            if((windowHeight+scrollPos)>thisPos){
+                $(this).addClass('fadeIn');
+            }
+        })
+
+        $('#profiles').css('background-position=y',(scrollPos/2)+'px');
+        $('#header-ele').css('transform', 'translateY( ' + (scrollPos / 2) + 'px )');
+        // $('#header-ele').css('transform', 'translateY( ' + (scrollPos / 2) + 'px )');
+  });
+
+    }); 
